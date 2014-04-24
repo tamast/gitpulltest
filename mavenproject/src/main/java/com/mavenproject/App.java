0e26667c379c9e72338d5c0d2453a14b2626f0a3
Tamas Takacs <tamas_takacs@epam.com>
2014-04-24 09:19:46 +0000

    init

---
+++ mavenproject/src/main/java/com/mavenproject/App.java
@@ -0,0 +1,13 @@
+package com.mavenproject;
+
+
+public class App {
+
+    public static void main(final String[] args) {
+        int i = 1;
+        int j = 1;
+        int k = 1;
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ mavenproject/src/test/java/com/mavenproject/AppTest.java
@@ -0,0 +1,16 @@
+package com.mavenproject;
+
+import org.junit.Test;
+//import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        App.main(args);
+        //fail( "The test case is a prototype." );
+    }
+
+}
